773ec4c2ee08b2828f28d56d1e0cbe8bd3085a30
Imavicks <victor.imazobi@gmail.com>
2023-09-26 21:07:49 +0100

    test the catched DOM elements on the console

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const computerScore = 0;
 const userScore_span = document.getElementById('userScore');
 const computerScore_span = document.getElementById('compScore');
 const scoreBoard_div = document.querySelector('.scoreBoard');
+const result_div = document.querySelector('.result');
 const rock_div = document.getElementById('rock');
 const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors');
+
+rock_div.addEventListener('click', function() {
+    console.log('hey you clicked on rock');
+});
