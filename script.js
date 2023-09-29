5364dc9704efd4d1d99ddd14b08720f93b4a743f
Imavicks <victor.imazobi@gmail.com>
2023-09-29 02:00:14 +0100

    create functions for lose and draw

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('userScore');
 const computerScore_span = document.getElementById('compScore');
 const scoreBoard_div = document.querySelector('.scoreBoard');
-const result_div = document.querySelector('.result');
+const result_p = document.querySelector('.result > p');
 const rock_div = document.getElementById('rock');
 const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors');
@@ -14,23 +14,49 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
+function win (userChoice, computerChoice) {
+    userScore += 1;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "(user)".fontsize(3).sup();
+    const smallCompWord = "(comp)".fontsize(3).sup();
+    result_p.innerHTML = userChoice + smallUserWord + " beats " + computerChoice + smallCompWord + " . You win!";
+}
+
+function lose (userChoice, computerChoice) {
+    computerScore += 1;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "(user)".fontsize(3).sup();
+    const smallCompWord = "(comp)".fontsize(3).sup();
+    result_p.innerHTML = userChoice + smallUserWord + " loses to  " + computerChoice + smallCompWord + " . You lost!";
+}
+
+function draw (userChoice, computerChoice) {
+    const smallUserWord = "(user)".fontsize(3).sup();
+    const smallCompWord = "(comp)".fontsize(3).sup();
+    result_p.innerHTML = userChoice + smallUserWord + " equals " + computerChoice + smallCompWord + " . its a draw!";
+}
+
+
+
 function game(userChoice) {
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
         case "rockscissors":
         case "paperrock":
         case "scissorspaper":
-            console.log('User wins');
+            win(userChoice, computerChoice);
             break;
         case "rockpaper":
         case "paperscissors":
         case 'scissorsrock':
-            console.log('computer wins');
+            lose(userChoice, computerChoice);
             break;
         case "rockrock":
         case "paperpaper":
         case 'scissorscisors':
-            console.log('Its a draw');
+            draw(userChoice, computerChoice);
             break;
     }
 }
